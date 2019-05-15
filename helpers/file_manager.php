a77e94d981862f77db17b3ba93240623a11d222e
macrailmmurray <matthew.murray@macrail.co.uk>
2019-05-15 16:33:14 +0100

    rename upload helper and add file deletion

--- helpers/file_manager.php
+++ helpers/file_manager.php
@@ -1,6 +1,6 @@
 <?php // IPS-CORE Uploader
 
-class ipsCore_uploader
+class ipsCore_file_manager
 {
 
     public static $upload_directory = 'uploads/';
@@ -25,33 +25,36 @@ class ipsCore_uploader
     {
         $errors = [];
 
-        $raw_file = ipsCore_uploader::get_sent_file($file_name);
-        $raw_file['uploadto'] = ipsCore_uploader::$upload_directory . $raw_file['name'];
+        $raw_file = ipsCore_file_manager::get_sent_file($file_name);
+        $raw_file['uploadto'] = ipsCore_file_manager::$upload_directory . $raw_file['name'];
         $raw_file['extension'] = strtolower(pathinfo($raw_file['name'], PATHINFO_EXTENSION));
-        $raw_file['basename'] = basename($raw_file['uploadto'],"." . $raw_file['extension']);
+        $raw_file['basename'] = basename($raw_file['uploadto'], "." . $raw_file['extension']);
 
         if ($type == 'image') {
-            ipsCore_uploader::validate_image($raw_file, $errors);
+            ipsCore_file_manager::validate_image($raw_file, $errors);
         } elseif ($type == 'file') {
-            ipsCore_uploader::validate_file($raw_file, $errors);
+            ipsCore_file_manager::validate_file($raw_file, $errors);
         }
 
         if (empty($errors)) {
-            if (file_exists(ipsCore_uploader::$upload_directory)) {
+            if (file_exists(ipsCore_file_manager::$upload_directory)) {
                 $i = 2;
                 $uploadto = $raw_file['uploadto'];
                 while (file_exists($uploadto)) {
-                    $uploadto = ipsCore_uploader::$upload_directory . $raw_file['basename'] . '-' . $i . '.' . $raw_file['extension'];
+                    $raw_file['basename'] = $raw_file['basename'] . '-' . $i;
+                    $raw_file['filename'] = $raw_file['basename'] . '.' . $raw_file['extension'];
+                    $uploadto = ipsCore_file_manager::$upload_directory . $raw_file['filename'];
+
                     $i++;
                 }
                 $raw_file['uploadto'] = $uploadto;
 
-                if ($raw_file["size"] <= ipsCore_uploader::$max_upload_size) {
+                if ($raw_file["size"] <= ipsCore_file_manager::$max_upload_size) {
                     if (!move_uploaded_file($raw_file["tmp_name"], $raw_file['uploadto'])) {
                         $errors[] = 'There was a problem moving the uploaded file.';
                     }
                 } else {
-                    $errors[] = 'That file is too large, max upload size is: ' . ipsCore_uploader::$max_upload_size . ' bytes.';
+                    $errors[] = 'That file is too large, max upload size is: ' . ipsCore_file_manager::$max_upload_size . ' bytes.';
                 }
             } else {
                 $errors[] = 'The uploads directory does not appear to exist.';
@@ -63,9 +66,7 @@ class ipsCore_uploader
 
     public static function validate_file($raw_file, &$errors)
     {
-        $data = false;
-
-        if (in_array($raw_file['extension'], ipsCore_uploader::$allowed_types_files)) {
+        if (in_array($raw_file['extension'], ipsCore_file_manager::$allowed_types_files)) {
             $check = getimagesize($raw_file["tmp_name"]);
 
             if ($check === false) {
@@ -78,7 +79,7 @@ class ipsCore_uploader
 
     public static function validate_image(&$raw_file, &$errors)
     {
-        if (in_array($raw_file['extension'], ipsCore_uploader::$allowed_types_images)) {
+        if (in_array($raw_file['extension'], ipsCore_file_manager::$allowed_types_images)) {
             $data = getimagesize($raw_file["tmp_name"]);
 
             if ($data === false) {
@@ -90,5 +91,14 @@ class ipsCore_uploader
         } else {
             $errors[] = 'Uploaded Image type is not allowed.';
         }
+    }
+
+    public static function do_delete_file($filename)
+    {
+        if (unlink(ipsCore_file_manager::$upload_directory . $filename)) {
+            return true;
+        }
+
+        return false;
     }
 }
